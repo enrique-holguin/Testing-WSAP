65aed1499b2ca3a60d6afc408f13b73421384529
Enrique <e.holguin993@gmail.com>
2023-07-07 21:24:51 -0500

    API Express

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+//acceder a las variables de entorno .env
+require("dotenv").config();
 const { createBot, createProvider, createFlow } = require("@bot-whatsapp/bot");
 
 const QRPortalWeb = require("@bot-whatsapp/portal");
 const BaileysProvider = require("@bot-whatsapp/provider/baileys");
 const MockAdapter = require("@bot-whatsapp/database/mock");
-const BOTNAME = "Cremoladas-Bot";
-const BOTPORT = 3001;
+const BOTNAME = process.env.BOT_NAME;
+const BOTPORT = process.env.BOT_PORT;
+const express = require("express");
+const { join } = require("path");
+const { createReadStream } = require("fs");
 
 //Flows
 const flowMain = require("./flows/flowMain");
@@ -14,6 +19,7 @@ const flowAdmin = require("./flows/flowPurchase");
 const flowCatalogo = require("./flows/flowTest2");
 const { flowForm } = require("./flows/flowTest3");
 
+const app = express();
 const main = async () => {
   const adapterDB = new MockAdapter();
   const adapterFlow = createFlow([
@@ -30,7 +36,15 @@ const main = async () => {
     database: adapterDB,
   });
 
-  QRPortalWeb({ name: BOTNAME, port: BOTPORT });
+  app.get("/get-qr", async (req, res) => {
+    const YOUR_PATH_QR = join(process.cwd(), `bot.qr.png`);
+    const fileStream = createReadStream(YOUR_PATH_QR);
+
+    res.writeHead(200, { "Content-Type": "image/png" });
+    fileStream.pipe(res);
+  });
+
+  app.listen(BOTPORT, () => console.log(`http://localhost:${BOTPORT}`));
 };
 
 main();
