30345a7577d46c96e2f2d055b814eb6b6cb52e88
Enrique <e.holguin993@gmail.com>
2023-07-09 22:58:14 -0500

    Cambios en flowMain

--- flows/flowMain.js
+++ flows/flowMain.js
@@ -1,5 +1,25 @@
 const { addKeyword, EVENTS } = require("@bot-whatsapp/bot");
 
-const flowMain = addKeyword(EVENTS.WELCOME).addAnswer("Bienvenido");
+//Servicios y Repistorios
+const { UserRepository } = require("../api/User/repositories/UserRepository");
+const { UserService } = require("../api/User/services/UserService");
+
+//Inyección de dependencias
+const userRepository = new UserRepository();
+const userService = new UserService(userRepository);
+
+//Mensajes
+const { messages } = require("../utils/messages/flowMain");
+
+const flowMain = addKeyword(EVENTS.WELCOME).addAction(
+  async (ctx, { flowDynamic, endFlow, gotoFlow }) => {
+    const { from: phone, pushName: defaultName } = ctx;
+    const user = userService.getUser(phone);
+    if (!user) {
+      await flowDynamic(messages.welcome(defaultName));
+    }
+    await flowDynamic(messages.welcome(user.name));
+  }
+);
 
 module.exports = { flowMain };
--- utils/messages/flowMain.js
+++ utils/messages/flowMain.js
@@ -1,1 +1,7 @@
-const messages = Object.freeze({})
+const messages = Object.freeze({
+  welcome(name) {
+    return `Bienvenido ${name}!`;
+  },
+});
+
+module.exports = { messages };
